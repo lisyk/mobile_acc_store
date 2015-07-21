8bba9e7a5c0101957ddcb6c9ede2483be38fe524
Lisyk <sergey.liskovoy@gmail.com>
2015-07-20 21:33:39 -0400

    set cart env

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,8 +2,12 @@ class Cart
   attr_reader :items
 
   def self.build_from_hash(hash)
-    items = hash["cart"]["items"].map do |item_data|
+    items = if hash["cart"] then
+      hash["cart"]["items"].map do |item_data|
       CartItem.new item_data["product_id"], item_data["quantity"]
+    end
+    else
+      []
     end
     new items
   end
